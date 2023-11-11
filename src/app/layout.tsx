867417423c3bd35f1c70e549e512355776e3648c
estebantosoni <tosoniesteban@gmail.com>
2023-11-11 11:53:29 -0300

    add navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Navbar from './components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-white text-black dark:bg-[#090908] dark:text-white h-full selection:bg-gray-50 dark:selection:bg-gray-800`}>
+        <Navbar />
+        <main className='max-w-6xl mx-auto px-4 sm:px-6 lg:px-8'>
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
